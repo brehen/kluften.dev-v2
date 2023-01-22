4cc43370ed9f1fffeb4fd9a7944689dd9e62779e
brehen <mkluften93@gmail.com>
2023-01-22 20:30:09 +0100

    🎨 Add tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,70 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+const plugin = require('tailwindcss/plugin')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	daisyui: {
+		themes: [
+			{
+				kluften: {
+					primary: '#2c4c7f',
+					secondary: '#1a338f',
+					accent: '#ff3c9c',
+					neutral: '#010101',
+					'base-100': '#E9E0EB',
+					info: '#AACEE9',
+					success: '#15605A',
+					warning: '#ef591e',
+					error: '#F31651'
+				}
+			},
+			{
+				'kluften-dark': {
+					primary: '#2c4c7f',
+					secondary: '#1a338f',
+					accent: '#ff3c9c',
+					'accent-alt': '',
+					'base-100': '#2A303C',
+					'base-content': '#d6ddca',
+					'primary-content': '#3d404c',
+					info: '#AACEE9',
+					success: '#15605A',
+					warning: '#d95318',
+					error: '#F31651'
+				}
+			}
+		]
+	},
+	theme: {
+		screens: defaultTheme.screens,
+		extend: {
+			fontFamily: {
+				sans: ['Overpass', ...defaultTheme.fontFamily.sans],
+				fancy: ['Poppins']
+			},
+			screens: {
+				'3xl': '2560px'
+			},
+			colors: {
+				main: '#16296e',
+				secondary: '#1a338f',
+				tertiary: '#236ada',
+				fortiary: '#E5EFFF',
+				accent: '#ff3c9c',
+				'accent-alt': '#dd5d2e'
+			}
+		}
+	},
+	plugins: [
+		require('@tailwindcss/typography'),
+		require('daisyui'),
+		plugin(function ({ addUtilities }) {
+			addUtilities({
+				'.rotate-x-180': {
+					transform: 'rotateX(180deg)'
+				}
+			})
+		})
+	]
+}
