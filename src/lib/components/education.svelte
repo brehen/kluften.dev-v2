1853b9b32b5ff689713ac42f3b64e6714f71a764
Marius Kluften <mkluften93@gmail.com>
2023-05-27 21:11:39 +0200

    seperate out education

---
+++ src/lib/components/education.svelte
@@ -0,0 +1,26 @@
+<script>
+	import Sectionwrapper from './sectionwrapper.svelte'
+</script>
+
+<Sectionwrapper>
+	<div
+		class="w-full prose prose-h2:!text-primary-900
+prose-h3:!text-tertiary-600-300-token prose-h2:font-fancy"
+	>
+		<h2 class="section-title bg-success-400">Education</h2>
+		<div class="mx-6 border-b-2 border-dashed border-primary-800">
+			<h3 class="flex justify-between mt-6">
+				Master, Informatics (part-time)
+				<span>2022 - current</span>
+			</h3>
+			<p class="">University of Oslo</p>
+		</div>
+		<div class="px-6">
+			<h3 class="flex justify-between mt-4">
+				Bachelor, Software Engineering
+				<span>2014 - 2017</span>
+			</h3>
+			<p>Oslo and Akershus University College of Applied Sciences</p>
+		</div>
+	</div>
+</Sectionwrapper>
