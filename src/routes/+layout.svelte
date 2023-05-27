23023592029bdf9a094d72ea10bfaf41464b42c9
Marius Kluften <mkluften93@gmail.com>
2023-05-27 21:11:00 +0200

    add skeleton and remove gdpr issue with fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,51 @@
 <script>
+	// Your selected Skeleton theme:
+	import '../kluften.css'
+	import { autoModeWatcher } from '@skeletonlabs/skeleton'
+	// This contains the bulk of Skeletons required styles:
+	// NOTE: this will be renamed skeleton.css in the v2.x release.
+	import '@skeletonlabs/skeleton/styles/skeleton.css'
+
+	// Finally, your application's global stylesheet (sometimes labeled 'app.css')
 	import '../app.css'
 </script>
 
 <svelte:head>
 	<link
-		href="https://fonts.googleapis.com/css2?family=Overpass:wght@300&family=Poppins:wght@300;400;500&display=swap"
-		rel="stylesheet"
+		rel="preload"
+		href="/fonts/Overpass-VariableFont_wght.ttf"
+		as="font"
+		type="font/ttf"
+		crossorigin="anonymous"
 	/>
+	<link
+		rel="preload"
+		href="/fonts/Overpass-Italic-VariableFont_wght.ttf"
+		as="font"
+		type="font/ttf"
+		crossorigin="anonymous"
+	/>
+	<link
+		rel="preload"
+		href="/fonts/Poppins-Light.ttf"
+		as="font"
+		type="font/ttf"
+		crossorigin="anonymous"
+	/>
+	<link
+		rel="preload"
+		href="/fonts/Poppins-Medium.ttf"
+		as="font"
+		type="font/ttf"
+		crossorigin="anonymous"
+	/>
+	<link
+		rel="preload"
+		href="/fonts/Poppins-Regular.ttf"
+		as="font"
+		type="font/ttf"
+		crossorigin="anonymous"
+	/>
+	{@html `<script>${autoModeWatcher.toString()} autoModeWatcher();</script>`}
 </svelte:head>
 <slot />
