bf78d73373b47edcaabb34d0807817fb0d6913bb
Marius Kluften <mkluften93@gmail.com>
2023-05-27 23:23:24 +0200

    important update

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,25 @@
+<script>
+	import Logo from '$lib/assets/svgs/logo.svelte'
+	import { AppBar, LightSwitch } from '@skeletonlabs/skeleton'
+</script>
+
+<AppBar>
+	<svelte:fragment slot="lead">
+		<a href="/" class="flex gap-2 items-center !text-primary-900-50-token">
+			<Logo />
+			kluften.dev
+		</a>
+	</svelte:fragment>
+	<svelte:fragment slot="trail">
+		<div class="flex !text-primary-900-50-token gap-8">
+			<a href="/blog">blog</a>
+			<LightSwitch />
+		</div>
+	</svelte:fragment>
+</AppBar>
+
+<style>
+  :global(.app-bar) {
+    @apply fixed w-full z-[999];
+  }
+</style>
---
+++ src/routes/+error.svelte
@@ -0,0 +1,17 @@
+<script>
+	import linux from '$lib/assets/linux-bordered.png'
+	import gatsby from '$lib/assets/gatsby-bordered-box.png'
+	import neko from '$lib/assets/neko-bordered-web.png'
+</script>
+
+<div class="flex justify-center items-center w-dscreen h-dscreen">
+	<div class="flex flex-col gap-10 items-center">
+		<h1 class="h1">404</h1>
+		<span>There be no blog here yet, have some pets</span>
+		<div class="flex overflow-hidden gap-4 rounded-lg bg-tertiary-300">
+			<img src={gatsby} alt="Gato numero uno" class="w-40 h-40" />
+			<img src={neko} alt="Gato numero tres" class="w-40 h-40" />
+			<img src={linux} alt="Gato numero uno" class="w-40 h-40" />
+		</div>
+	</div>
+</div>
