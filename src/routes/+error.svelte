e5d2f9441ddb200e75e7d7e6b8c56dd2a8824825
Marius Kluften <mkluften93@gmail.com>
2023-05-27 23:28:57 +0200

    responsive

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -9,9 +9,9 @@
 		<h1 class="h1">404</h1>
 		<span>There be no blog here yet, have some pets</span>
 		<div class="flex overflow-hidden gap-4 rounded-lg bg-tertiary-300">
-			<img src={gatsby} alt="Gato numero uno" class="w-40 h-40" />
-			<img src={neko} alt="Gato numero tres" class="w-40 h-40" />
-			<img src={linux} alt="Gato numero uno" class="w-40 h-40" />
+			<img src={gatsby} alt="Gato numero uno" class="w-24 h-24 md:w-40 md:h-40" />
+			<img src={neko} alt="Gato numero tres" class="w-24 h-24 md:w-40 md:h-40" />
+			<img src={linux} alt="Gato numero uno" class="w-24 h-24 md:w-40 md:h-40" />
 		</div>
 	</div>
 </div>
