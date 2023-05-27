250e9bf662ad06fcec7178947c60b824b659fe92
Marius Kluften <mkluften93@gmail.com>
2023-05-27 23:26:07 +0200

    fix

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -8,7 +8,7 @@
 	<div class="flex flex-col gap-10 items-center">
 		<h1 class="h1">404</h1>
 		<span>There be no blog here yet, have some pets</span>
-		<div class="flex overflow-hidden gap-4 rounded-lg bg-tertiary-300">
+		<div class="flex overflow-hidden flex-col gap-4 rounded-lg md:flex-row bg-tertiary-300">
 			<img src={gatsby} alt="Gato numero uno" class="w-40 h-40" />
 			<img src={neko} alt="Gato numero tres" class="w-40 h-40" />
 			<img src={linux} alt="Gato numero uno" class="w-40 h-40" />
