7546b53f483182f2276ff658e1fb7646bc382817
Marius Kluften <mkluften93@gmail.com>
2023-05-28 11:49:15 +0200

    animate pets safely

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -9,9 +9,21 @@
 		<h1 class="h1">404</h1>
 		<span>There be no blog here yet, have some pets</span>
 		<div class="flex overflow-hidden gap-4 rounded-lg bg-tertiary-300">
-			<img src={gatsby} alt="Gato numero uno" class="w-24 h-24 md:w-40 md:h-40" />
-			<img src={neko} alt="Gato numero tres" class="w-24 h-24 md:w-40 md:h-40" />
-			<img src={linux} alt="Gato numero uno" class="w-24 h-24 md:w-40 md:h-40" />
+			<img
+				src={gatsby}
+				alt="Gato numero uno"
+				class="w-24 h-24 md:w-40 md:h-40 motion-safe:animate-pulse"
+			/>
+			<img
+				src={neko}
+				alt="Gato numero tres"
+				class="w-24 h-24 md:w-40 md:h-40 motion-safe:animate-pulse"
+			/>
+			<img
+				src={linux}
+				alt="Gato numero uno"
+				class="w-24 h-24 md:w-40 md:h-40 motion-safe:animate-pulse"
+			/>
 		</div>
 	</div>
 </div>
