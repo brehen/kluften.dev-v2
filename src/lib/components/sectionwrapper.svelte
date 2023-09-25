26cc223b990f3604f09a949f4e3b1d78a7589ba6
Marius Kluften <mkluften93@gmail.com>
2023-09-25 11:31:49 +0200

    Update background color on sections

--- src/lib/components/sectionwrapper.svelte
+++ src/lib/components/sectionwrapper.svelte
@@ -6,7 +6,7 @@
 	<section class="flex justify-center w-full">
 		<div class="flex p-1.5 w-full max-w-prose md:w-3/4 md:rounded-xl">
 			<div
-				class="overflow-hidden w-full rounded-2xl border-2 border-b-[10px] border-l-[17px] border-primary-900 bg-surface-200-700-token"
+				class="overflow-hidden w-full rounded-2xl border-2 border-b-[10px] border-l-[17px] border-primary-900 bg-white dark:bg-surface-700"
 			>
 				<slot />
 			</div>
