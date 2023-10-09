3a2d23fd0c609b6017025f32fdcadeccf935ff98
Marius Kluften <marius@kluften.dev>
2023-10-09 16:04:31 +0200

    add 404 page

--- src/routes/blog/[slug]/+page.server.ts
+++ src/routes/blog/[slug]/+page.server.ts
@@ -1,7 +1,17 @@
 // src/routes/blog/[...slug].ts
 import type { PageServerLoad } from './$types'
+import blog_meta from '$lib/data/blog_meta'
+import { error } from '@sveltejs/kit'
 
 export const load: PageServerLoad = async ({ fetch, params }) => {
+	const postIsPublished = blog_meta.find(({ slug }) => slug === params.slug)
+
+	if (!postIsPublished) {
+		throw error(404, {
+			message:
+				'<p>Blog post not found</p> <a href="/blog" class="anchor text-warning-600-300-token">Click here to go back</a>'
+		})
+	}
 	const post = await fetch(`/blog-posts/${params.slug}.md`)
 	const content = await post.text()
 
