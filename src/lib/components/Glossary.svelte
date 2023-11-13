abf81ed9ba1ec1e68286f0ef80abadbc6e9ad8f1
Marius Kluften <marius@kluften.dev>
2023-11-13 23:57:00 +0100

    add glossary component

---
+++ src/lib/components/Glossary.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { Accordion, AccordionItem } from '@skeletonlabs/skeleton'
+
+	export let glossary: {
+		word: string
+		description: string
+	}[]
+</script>
+
+{#if glossary}
+	<div class="rounded-lg border border-l-8 border-primary-300 bg-slate-300 dark:marker:text-black">
+		<Accordion>
+			<AccordionItem>
+				<svelte:fragment slot="summary">
+					<span class="text-2xl mt-2 relative block">Glossary</span>
+				</svelte:fragment>
+				<svelte:fragment slot="content">
+					<ol>
+						{#each glossary as item}
+							<li><b>{item.word}</b>: {item.description}</li>
+						{/each}
+					</ol>
+				</svelte:fragment>
+			</AccordionItem>
+		</Accordion>
+	</div>
+{/if}
