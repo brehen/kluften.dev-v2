680e144d541e4d0266eedabcf199ba59b39251b2
Marius Kluften <marius@kluften.dev>
2025-03-03 19:56:42 +0100

    fix: replace old accordion with new one

--- src/lib/components/Glossary.svelte
+++ src/lib/components/Glossary.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { Accordion, AccordionItem } from '@skeletonlabs/skeleton'
+	import { Accordion } from '@skeletonlabs/skeleton-svelte'
 
 	export let glossary: {
 		word: string
@@ -9,19 +9,20 @@
 
 {#if glossary}
 	<div class="rounded-lg border border-l-8 border-primary-300 bg-slate-300 dark:marker:text-black">
-		<Accordion>
-			<AccordionItem>
-				<svelte:fragment slot="summary">
+		<Accordion collapsible>
+			<Accordion.Item value="glossary">
+				{#snippet control()}
+					<span class="text-lg">During my time I've helped these customers:</span>
 					<span class="text-2xl mt-2 relative block">Glossary</span>
-				</svelte:fragment>
-				<svelte:fragment slot="content">
+				{/snippet}
+				{#snippet panel()}
 					<ol>
 						{#each glossary as item (item.word)}
 							<li><b>{item.word}</b>: {item.description}</li>
 						{/each}
 					</ol>
-				</svelte:fragment>
-			</AccordionItem>
+				{/snippet}
+			</Accordion.Item>
 		</Accordion>
 	</div>
 {/if}
