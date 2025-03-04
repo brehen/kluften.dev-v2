e086ea4299f04ef59699a95a38186fb6c59e63d3
Marius Kluften <marius@kluften.dev>
2025-03-04 09:03:02 +0100

    feat: handle blog

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,19 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+    prerender: {
+      handleHttpError: ({ path, referrer, message }) => {
+        // If it's a link to a future blog post, we can ignore it
+        if (path.includes('/blog/nebula_chapter') && referrer === '/blog') {
+          console.warn(`Ignoring missing future blog post: ${path}`);
+          return;
+        }
+        
+        // Otherwise, treat it as a real error
+        throw new Error(message);
+      }
+    }
 	},
   lias: {
 		$lib: 'src/lib'
