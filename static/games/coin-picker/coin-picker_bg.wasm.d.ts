eea309976a247a39d5f6e470ea9f46c0b1171e92
Marius Kluften <marius@kluften.dev>
2026-03-20 13:00:55 +0100

    feat: assets

---
+++ static/games/coin-picker/coin-picker_bg.wasm.d.ts
@@ -0,0 +1,27 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const wasm_main: () => void;
+export const main: (a: number, b: number) => number;
+export const wasm_bindgen__closure__destroy__h397b4a60abc39179: (a: number, b: number) => void;
+export const wasm_bindgen__closure__destroy__h347d98261c33cbaf: (a: number, b: number) => void;
+export const wasm_bindgen__convert__closures_____invoke__h61fa9a9adbb03c14: (a: number, b: number, c: any, d: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h91f16fb2f5e2fa96: (a: number, b: number, c: any) => [number, number];
+export const wasm_bindgen__convert__closures_____invoke__h03bbb1fa049a4ade: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h03bbb1fa049a4ade_2: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h03bbb1fa049a4ade_3: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h03bbb1fa049a4ade_4: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h03bbb1fa049a4ade_5: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h03bbb1fa049a4ade_6: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h03bbb1fa049a4ade_7: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h03bbb1fa049a4ade_8: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__hac49a9c12578d319: (a: number, b: number, c: number) => void;
+export const wasm_bindgen__convert__closures_____invoke__hfb746d3413cbab83: (a: number, b: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
